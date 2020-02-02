048224ed84198e4e6297fea47eed82ff724cee8c
Jessica Sarlin <jessica.sarlin@gmail.com>
2020-02-02 19:42:24 +0200

    Small change to cause merge conflict

--- src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -51,7 +51,7 @@ public class Varasto {
     public void lisaaVarastoon(double maara) {
         if (maara < 0) // virhetilanteessa voidaan tehdä 
         {
-            return;       // tällainen pikapoistuminenkin!
+            return;       // tällainen pikapoistuminenkin
         }
         if (maara <= paljonkoMahtuu()) // omia aksessoreita voi kutsua
         {
